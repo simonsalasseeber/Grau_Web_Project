940dc2be7fe71f6e78e5979ff680387a24a90bd8
simonsalasseeber@gmail.com <simonsalasseeber@gmail.com>
2024-05-13 16:22:27 +0200

    added suspense

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -4,16 +4,20 @@ import Home from './views/Home/Home'
 import Admin from './views/Admin/Admin'
 import About from './views/About/About'
 import { Routes, Route } from 'react-router-dom';
+import React from "react";
+
 
 function App() {
 
   return (
     <>
-    <Routes>
-        <Route path="/" element={<Home/>} />
-        <Route path="/admin" element={<Admin/>} />
-        <Route path="/about" element={<About/>} />
-    </Routes>
+    <React.Suspense fallback={<img src="./assets/LogoGrau_Vectorial_1.svg" alt="Logo" />}>
+      <Routes>
+          <Route path="/" element={<Home/>} />
+          <Route path="/admin" element={<Admin/>} />
+          <Route path="/about" element={<About/>} />
+      </Routes>
+    </React.Suspense>
       
     </>
   )
