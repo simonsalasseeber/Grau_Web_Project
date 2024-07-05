fe34aa5db119db8ac04387cb6f08d91ca851e192
simonsalasseeber <simonsalasseeber@gmail.com>
2024-07-05 13:25:40 +0200

    styling hurts

--- front/src/components/NavBar/NavBar.jsx
+++ front/src/components/NavBar/NavBar.jsx
@@ -12,13 +12,13 @@ const NavBar = () => {
 
 <div className="flex flex-col w-full h-auto items-center justify-between pt-16 pb-5 z-1">
 <div className="relative z-100 w-full max-w-4xl h-auto flex justify-center mt-2 mb-0 px-4">
-  <ReactSVG src={reactLogo} className="h-auto w-2/5 max-w-xl " />
+  <ReactSVG src={reactLogo} className="h-auto w-3/5 max-w-xl " />
 </div>
-<div className='flex grid-cols-3 space-evenly gap-3 items-end text-[#E0DECD] font-atle-haas-bold md:text-xs' >
-    <p>ORIGINAL MUSIC</p>
-    <p>MIXING</p>
-    <p>SOUND DESIGN</p>
-</div>
+<div className="w-11/12 flex flex-row justify-between items-end px-4 md:px-8 lg:px-16">
+      <span className="font-atle-haas-bold text-[#E0DECD] text-sm sm:text-base md:text-lg">ORIGINAL MUSIC</span>
+      <span className="font-atle-haas-bold text-[#E0DECD] text-sm sm:text-base md:text-lg">MIXING</span>
+      <span className="font-atle-haas-bold text-[#E0DECD] text-sm sm:text-base md:text-lg">SOUND DESIGN</span>
+    </div>
 </div>
 
 
